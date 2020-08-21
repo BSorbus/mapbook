c6838042eae8c40da5e7e26b0ea19908ad5a75ad
BSorbus <BSorbus@gmail.com>
2020-08-21 11:34:05 +0200

    Better routes for api_teryt

--- app/controllers/api_teryt_controller.rb
+++ app/controllers/api_teryt_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class ApiTerytController < ApplicationController
 
-  def provinces_select2_index
+  def provinces
     items_obj = ApiTerytProvince.new(q: "#{params[:q]}", page: "#{params[:page]}", page_limit: "#{params[:page_limit]}")
     if items_obj.request_for_collection # return true
       render json: JSON.parse(items_obj.response.body), status: items_obj.response.code
@@ -14,7 +14,7 @@ class ApiTerytController < ApplicationController
     end
   end
 
-  def province_districts_select2_index
+  def province_districts
     items_obj = ApiTerytProvinceDistricts.new(id: params[:province_id], q: "#{params[:q]}", page: "#{params[:page]}", page_limit: "#{params[:page_limit]}")
     if items_obj.request_for_collection # return true
       render json: JSON.parse(items_obj.response.body), status: items_obj.response.code
@@ -27,7 +27,7 @@ class ApiTerytController < ApplicationController
     end
   end
 
-  def district_communes_select2_index
+  def district_communes
     items_obj = ApiTerytDistrictCommunes.new(id: params[:district_id], q: "#{params[:q]}", page: "#{params[:page]}", page_limit: "#{params[:page_limit]}")
     if items_obj.request_for_collection # return true
       render json: JSON.parse(items_obj.response.body), status: items_obj.response.code
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get  '/teryt/:teryt/file_pdf', to: 'teryt#file_pdf'
   get  '/teryt/:teryt/file_xlsx', to: 'teryt#file_xlsx'
 
-  get '/api_teryt/provinces_select2_index',          to: 'api_teryt#provinces_select2_index'
-  get '/api_teryt/province_districts_select2_index', to: 'api_teryt#province_districts_select2_index'
-  get '/api_teryt/district_communes_select2_index',  to: 'api_teryt#district_communes_select2_index'
+  get '/api_teryt/provinces',          to: 'api_teryt#provinces'
+  get '/api_teryt/province_districts', to: 'api_teryt#province_districts'
+  get '/api_teryt/district_communes',  to: 'api_teryt#district_communes'
 
   post '/areas/find_or_create', to: 'areas#find_or_create'
   get  '/areas/info', to: 'areas#info'

99caf561a8ed935fe70c72b568c6d5469781a180
BSorbus <BSorbus@gmail.com>
2020-08-21 10:35:13 +0200

    Add better js and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get  '/teryt/:teryt/file_pdf', to: 'teryt#file_pdf'
   get  '/teryt/:teryt/file_xlsx', to: 'teryt#file_xlsx'
 
-  get '/apiteryt/provinces_select2_index',          to: 'api_teryt#provinces_select2_index'
-  get '/apiteryt/province_districts_select2_index', to: 'api_teryt#province_districts_select2_index'
-  get '/apiteryt/district_communes_select2_index',  to: 'api_teryt#district_communes_select2_index'
+  get '/api_teryt/provinces_select2_index',          to: 'api_teryt#provinces_select2_index'
+  get '/api_teryt/province_districts_select2_index', to: 'api_teryt#province_districts_select2_index'
+  get '/api_teryt/district_communes_select2_index',  to: 'api_teryt#district_communes_select2_index'
 
   post '/areas/find_or_create', to: 'areas#find_or_create'
   get  '/areas/info', to: 'areas#info'
